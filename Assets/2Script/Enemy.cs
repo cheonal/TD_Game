e80cd210399b8f7c0d48ec0e0aeca2172999cdea
cheonal <bulewing99@naver.com>
2023-01-31 22:28:37 +0900

    230131 Enemy Hp바

--- Assets/2Script/Enemy.cs
+++ Assets/2Script/Enemy.cs
@@ -1,26 +1,31 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Enemy : MonoBehaviour
 {
     public float startSpeed = 10f;
     [HideInInspector]
     public float speed;
 
-    public float health = 100;
-
+    public float startHelath = 100;
+    public float health;
     public int worth = 50;
 
     public GameObject deathEffect;
 
+    [Header("Unity Stuff")]
+    public Image HealthBar;
     void Start()
     {
+        health = startHelath;
         speed = startSpeed;
     }
     public void TakeDamage(float amount)
     {
         health -= amount;
 
-        if(health <= 0)
+        HealthBar.fillAmount = health / startHelath;
+
+        if (health <= 0)
         {
             Die();
         }

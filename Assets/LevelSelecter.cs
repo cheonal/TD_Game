622b531211b2458e00de938e2adf9a108b78f227
cheonal <bulewing99@naver.com>
2023-02-13 23:57:33 +0900

    230213 UI 제작중

---
+++ Assets/LevelSelecter.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class LevelSelecter : MonoBehaviour
+{
+    public SceneFader fader;
+
+    public void Select(string levelName)
+    {
+        fader.FadeTo(levelName);
+    }
+}
